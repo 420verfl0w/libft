b317ba7995fe0d6bc8feb95d5e876e3d3000b2c3
Unam3dd <skyrim.gaming16@gmail.com>
2022-02-17 20:08:17 +0100

    ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,20 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stales <stales@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/17 18:59:57 by pix               #+#    #+#             */
-/*   Updated: 2022/02/17 19:39:43 by stales           ###   ########.fr       */
+/*   Created: 2022/02/17 19:45:00 by stales            #+#    #+#             */
+/*   Updated: 2022/02/17 20:07:02 by stales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_toupper(int c)
+char	*ft_strchr(const char *s, int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (c ^ 0x20);
-	return (c);
+	char	*tmp;
+
+	tmp = (char *)s;
+	while (*tmp && *tmp != (char)c)
+		tmp++;
+	if (*tmp == c)
+		return (tmp);
+	return ((void *)0);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: stales <stales@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 19:23:22 by stales            #+#    #+#             */
-/*   Updated: 2022/02/17 19:24:07 by stales           ###   ########.fr       */
+/*   Updated: 2022/02/17 20:00:16 by stales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,12 @@
 
 typedef unsigned long	t_size;
 void	*ft_memset(void *s, int c, t_size n);
-void	ft_bzero(void *s, t_size n);
 void	*ft_memcpy(void *dest, const void *src, t_size n);
 void	*ft_memccpy(void *dest, const void *src, int c, t_size n);
 char	*ft_strcpy(char *dest, char *src);
+char	*ft_strchr(const char *s, int c);
 int		ft_strlen(char *str);
 int		ft_atoi(char *str);
-void	ft_putstr(char const *s);
-void	ft_putchar(char c);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -30,5 +28,8 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+void	ft_putstr(char const *s);
+void	ft_putchar(char c);
+void	ft_bzero(void *s, t_size n);
 
 #endif
