d3f28d6de07869cf86d5cf53a9b0eb5ffda305db
Pixailz <Pixailz1@gmail.com>
2022-02-18 02:29:54 +0100

    :art: libft.h, Makfile: sort function ascending

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: pix <pix@student.42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/02/17 18:22:12 by stales            #+#    #+#              #
-#    Updated: 2022/02/18 02:26:24 by pix              ###   ########.fr        #
+#    Updated: 2022/02/18 02:29:14 by pix              ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -25,40 +25,40 @@ reset			= \033[0m
 # **************************************************************************** #
 # config
 
-SRCS			=	ft_memset.c \
+SRCS			=	ft_atoi.c \
 					ft_bzero.c \
-					ft_memcpy.c \
-					ft_putstr.c \
-					ft_memccpy.c \
-					ft_strlen.c \
-					ft_strcpy.c \
-					ft_putchar.c \
-					ft_atoi.c \
-					ft_isdigit.c \
-					ft_isprint.c \
-					ft_isascii.c \
+					ft_calloc.c \
 					ft_isalnum.c \
 					ft_isalpha.c \
-					ft_tolower.c \
-					ft_toupper.c \
-					ft_strchr.c \
+					ft_isascii.c \
 					ft_isblank.c \
-					ft_strcmp.c \
-					ft_strncmp.c \
-					ft_strstr.c \
-					ft_strcat.c \
-					ft_strncpy.c \
+					ft_isdigit.c \
+					ft_isprint.c \
+					ft_memccpy.c \
 					ft_memchr.c \
-					ft_strdup.c \
-					ft_memmove.c \
-					ft_strncat.c \
-					ft_strnstr.c \
-					ft_calloc.c \
 					ft_memcmp.c \
-					ft_strlcpy.c \
+					ft_memcpy.c \
+					ft_memmove.c \
+					ft_memset.c \
+					ft_putchar.c \
+					ft_putnbr.c \
+					ft_putstr.c \
+					ft_strcat.c \
+					ft_strchr.c \
+					ft_strcmp.c \
+					ft_strcpy.c \
+					ft_strdup.c \
 					ft_strlcat.c \
+					ft_strlcpy.c \
+					ft_strlen.c \
+					ft_strncat.c \
+					ft_strncmp.c \
+					ft_strncpy.c \
+					ft_strnstr.c \
 					ft_strrchr.c \
-					ft_putnbr.c
+					ft_strstr.c \
+					ft_tolower.c \
+					ft_toupper.c
 OBJS			= $(SRCS:.c=.o)
 CFLAGS			= -Wall -Wextra -Werror -I.
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pix <pix@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 22:43:54 by stales            #+#    #+#             */
-/*   Updated: 2022/02/18 02:26:14 by pix              ###   ########.fr       */
+/*   Updated: 2022/02/18 02:28:22 by pix              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,39 +17,39 @@ typedef unsigned char	t_uint8;
 typedef unsigned int	t_uint32;
 typedef unsigned long	t_size;
 
-void		*ft_memset(void *s, int c, t_size n);
-void		*ft_memcpy(void *dest, const void *src, t_size n);
+void		*ft_calloc(t_size nmemb, t_size size);
 void		*ft_memccpy(void *dest, const void *src, int c, t_size n);
 void		*ft_memchr(const void *s, int c, t_size n);
+void		*ft_memcpy(void *dest, const void *src, t_size n);
 void		*ft_memmove(void *dest, const void *src, t_size n);
-void		*ft_calloc(t_size nmemb, t_size size);
-char		*ft_strcpy(char *dest, char *src);
-char		*ft_strncpy(char *dest, char *src, t_size n);
+void		*ft_memset(void *s, int c, t_size n);
+char		*ft_strcat(char *dest, char *src);
 char		*ft_strchr(const char *s, int c);
+char		*ft_strcpy(char *dest, char *src);
+char		*ft_strdup(char *src);
+char		*ft_strncat(char *dest, char *src, t_size n);
+char		*ft_strncpy(char *dest, char *src, t_size n);
+char		*ft_strnstr(char *str, char *to_find, int n);
 char		*ft_strrchr(const char *s, int c);
 char		*ft_strstr(char *str, char *to_find);
-char		*ft_strcat(char *dest, char *src);
-char		*ft_strncat(char *dest, char *src, t_size n);
-char		*ft_strdup(char *src);
-char		*ft_strnstr(char *str, char *to_find, int n);
 t_uint32	ft_strlcat(char *dest, char *src, t_size size);
 t_uint32	ft_strlcpy(char *dest, char *src, t_size size);
-int			ft_strlen(char *str);
 int			ft_atoi(char *str);
-int			ft_isalpha(int c);
-int			ft_isdigit(int c);
 int			ft_isalnum(int c);
+int			ft_isalpha(int c);
 int			ft_isascii(int c);
-int			ft_isprint(int c);
-int			ft_toupper(int c);
-int			ft_tolower(int c);
 int			ft_isblank(int c);
-int			ft_strcmp(char *s1, char *s2);
-int			ft_strncmp(char *s1, char *s2, t_size n);
+int			ft_isdigit(int c);
+int			ft_isprint(int c);
 int			ft_memcmp(const void *s1, const void *s2, t_size n);
-void		ft_putstr(char const *s);
-void		ft_putchar(char c);
+int			ft_strcmp(char *s1, char *s2);
+int			ft_strlen(char *str);
+int			ft_strncmp(char *s1, char *s2, t_size n);
+int			ft_tolower(int c);
+int			ft_toupper(int c);
 void		ft_bzero(void *s, t_size n);
+void		ft_putchar(char c);
 void		ft_putnbr(int nb);
+void		ft_putstr(char const *s);
 
 #endif
