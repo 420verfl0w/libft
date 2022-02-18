44ec0be21fbbd9a701b03efda28b4ada8939c060
Unam3dd <skyrim.gaming16@gmail.com>
2022-02-18 10:55:56 +0100

    ft_putnbr

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -3,35 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pix <pix@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: stales <stales@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 02:22:54 by pix               #+#    #+#             */
-/*   Updated: 2022/02/18 02:25:26 by pix              ###   ########.fr       */
+/*   Updated: 2022/02/18 10:55:39 by stales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <unistd.h>
 
 void	ft_putnbr(int nb)
 {
+	if (nb > 2147483647)
+		return ;
 	if (nb == -2147483648)
 	{
 		ft_putstr("-2147483648");
+		return ;
 	}
-	else
+	if (nb < 0)
 	{
-		if (nb < 0)
-		{
-			ft_putchar('-');
-			nb = -nb;
-		}
-		if (nb > 9)
-		{
-			ft_putnbr(nb / 10);
-			nb = nb % 10;
-		}
-		if (nb < 10)
-			ft_putchar('0' + nb);
+		ft_putchar('-');
+		nb = ~(nb - 1);
 	}
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	ft_putchar((nb % 10) + '0');
 }
