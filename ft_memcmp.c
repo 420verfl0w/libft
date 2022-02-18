3a341a87c446f97d0b256368145d9d1a69a1d945
Unam3dd <skyrim.gaming16@gmail.com>
2022-02-18 14:00:39 +0100

    update ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: stales <stales@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 00:00:23 by stales            #+#    #+#             */
-/*   Updated: 2022/02/18 00:04:51 by stales           ###   ########.fr       */
+/*   Updated: 2022/02/18 14:00:31 by stales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int	ft_memcmp(const void *s1, const void *s2, t_size n)
 	t_uint8	*ts1;
 	t_uint8	*ts2;
 
-	ts1 = (t_uint8 *)s1;
-	ts2 = (t_uint8 *)s2;
 	if (!n)
 		return (0);
+	ts1 = (t_uint8 *)s1;
+	ts2 = (t_uint8 *)s2;
 	while (*ts1 && *ts2 && --n && *ts1++ == *ts2)
 		ts2++;
 	return (*ts1 - *ts2);
