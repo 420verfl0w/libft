6d79ae61a257a3d6ff482ff40023b8373d8b702d
Pixailz <Pixailz1@gmail.com>
2022-02-19 15:31:15 +0100

    :pencil: write some documentation again

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,14 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stales <stales@42.fr>                      +#+  +:+       +#+        */
+/*   By: pix <pix@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 14:32:38 by stales            #+#    #+#             */
-/*   Updated: 2022/02/17 17:11:15 by stales           ###   ########.fr       */
+/*   Updated: 2022/02/19 14:22:33 by pix              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+ *	Desc:	The ft_memcpy() function copies n bytes from memory area src to
+ *			memory area dest.
+ *			The memory areas must not overlap.
+ *
+ *	*dest:	destination memory area
+ *	*src:	source memory area
+ *	n:		number of bytes to copy
+ *
+ *	Return:	The ft_memcpy() function returns a pointer to dest.
+ */
 
 void	*ft_memcpy(void *dest, const void *src, t_size n)
 {
--- ft_memmove.c
+++ ft_memmove.c
@@ -3,14 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stales <stales@42.fr>                      +#+  +:+       +#+        */
+/*   By: pix <pix@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 22:58:06 by stales            #+#    #+#             */
-/*   Updated: 2022/02/17 23:31:16 by stales           ###   ########.fr       */
+/*   Updated: 2022/02/19 14:26:06 by pix              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+ *	Desc:	The ft_memmove() function copies n bytes from memory area src to
+ *			memory area dest. The memory areas may overlap: copying takes place
+ *			as though the bytes in src are first copied into a temporary
+ *			array that does not overlap src or dest, and the bytes are then
+ *			copied from the temporary array to dest.
+ *
+ *	*dest:	destination memory area
+ *	*src:	source memory area
+ *	n:		number of bytes to copy
+ *
+ *	Return:	The ft_memmove() function returns a pointer to dest.
+ */
 
 void	*ft_memmove(void *dest, const void *src, t_size n)
 {
