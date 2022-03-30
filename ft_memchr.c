f5cd771c3b78b2085602ab9d706ee0eaa849fdae
Samuel Tales <stales@2G5.42angouleme.fr>
2022-03-30 17:49:43 +0200

    patch ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pix <pix@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: stales <stales@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 22:26:32 by stales            #+#    #+#             */
-/*   Updated: 2022/02/19 14:23:13 by pix              ###   ########.fr       */
+/*   Updated: 2022/03/30 17:49:34 by stales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,11 @@ void	*ft_memchr(const void *s, int c, t_size n)
 	t_uint8	*tmp;
 
 	tmp = (t_uint8 *)s;
-	while (--n && *tmp)
+	while (--n)
 	{
 		if (*tmp == (t_uint8)c)
 			return (tmp);
 		tmp++;
 	}
-	return ((void *)0);
+	return (LIBFT_NULL);
 }
