2530d3a5f9f9a63172766a120c52902d61c87432
Samuel Tales <stales@2G5.42angouleme.fr>
2022-03-31 10:57:18 +0200

    patch ft_strnew

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: stales <stales@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 13:05:07 by stales            #+#    #+#             */
-/*   Updated: 2022/03/10 13:09:23 by stales           ###   ########.fr       */
+/*   Updated: 2022/03/31 10:56:03 by stales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ char	*ft_strnew(t_size size)
 {
 	char	*ptr;
 
-	ptr = (char *)malloc(size);
-	ft_bzero((char *)ptr, size);
+	ptr = (char *)malloc(size + 1);
+	if (ptr)
+		ft_bzero((char *)ptr, size + 1);
 	return (ptr);
 }
