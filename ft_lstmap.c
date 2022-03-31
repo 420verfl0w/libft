5123192bf48b07f646a66dce20faafdf780e7b42
Samuel Tales <stales@2G5.42angouleme.fr>
2022-03-31 18:26:12 +0200

    update

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,20 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stales <stales@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/29 19:51:43 by stales            #+#    #+#             */
-/*   Updated: 2022/03/31 17:59:13 by stales           ###   ########.fr       */
+/*   Created: 2022/03/31 17:59:23 by stales            #+#    #+#             */
+/*   Updated: 2022/03/31 18:25:49 by stales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstlast(t_list *lst)
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	while (lst && lst->next)
+	t_list	*new;
+	t_list	*tmp;
+
+	new = LIBFT_NULL;
+	while (lst && f && del)
+	{
+		if (!(tmp = ft_lstnew(f(lst->content))))
+		{
+			ft_lstclear(&tmp, del);
+			return (LIBFT_NULL);
+		}
+		ft_lstadd_back(&new, tmp);
 		lst = lst->next;
-	return (lst);
+	}
+	return (new);
 }
