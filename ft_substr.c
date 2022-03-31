b601066447cc9e6fa1740b55544aeebbb2936b18
Bruno Da silva <brda-sil@1E4.42angouleme.fr>
2022-03-31 18:21:48 +0200

    :fire: ft_substr: patch

--- ft_substr.c
+++ ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stales <stales@42.fr>                      +#+  +:+       +#+        */
+/*   By: brda-sil <brda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 13:18:24 by stales            #+#    #+#             */
-/*   Updated: 2022/03/28 13:32:30 by stales           ###   ########.fr       */
+/*   Updated: 2022/03/31 18:20:58 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,20 @@
 
 char	*ft_substr(char const *s, unsigned int start, t_size len)
 {
-	char	*str;
+	char		*str;
+	t_size		i;
 
 	str = (char *)malloc(len + 1);
 	if (!str)
 		return (LIBFT_NULL);
-	ft_strncpy(str, (char *)(s + start), len);
-	str[len + 1] = 0;
+	if ((int)start > ft_strlen((char *)s))
+		return (ft_strdup(""));
+	i = 0;
+	while (i < len)
+	{
+		str[i] = *(s + start + i);
+		i++;
+	}
+	str[i] = 0;
 	return (str);
 }
