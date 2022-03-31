6d45e368342573cf49bee54b6df430e085ef2eca
Samuel Tales <stales@2G5.42angouleme.fr>
2022-03-31 18:49:47 +0200

    update:

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: stales <stales@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 15:25:32 by stales            #+#    #+#             */
-/*   Updated: 2022/03/31 18:47:02 by stales           ###   ########.fr       */
+/*   Updated: 2022/03/31 18:49:21 by stales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_itoa(long nb)
 	char		*nb_str;
 
 	nb_size = ft_nbrlen(nb);
-	nb_str = ft_calloc(0b1, nb_size | 0b1);
+	nb_str = ft_calloc(0b1, nb_size + 0b1);
 	if (nb < 0b0)
 	{
 		nb = ~(nb - 0b1);
