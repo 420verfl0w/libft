cb4f0b3890d54da0444befcd31b5fd7a2c950ea3
Samuel Tales <stales@2E7.42angouleme.fr>
2022-04-01 18:18:42 +0200

    FINAL PUSH DE LA MORT DE CETTE LIBFT DE MERDE PAS OPTIMIZED

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,27 +6,24 @@
 /*   By: stales <stales@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 12:08:31 by stales            #+#    #+#             */
-/*   Updated: 2022/03/29 15:29:12 by stales           ###   ########.fr       */
+/*   Updated: 2022/04/01 18:17:43 by stales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*end;
-	char	*start;
-	char	*ptr;
+	t_size	s;
+	t_size	i;
 
-	end = (char *)(s1 + ft_strlen((char *)s1) - 1);
-	start = (char *)s1;
-	while (*start && ft_strchr(set, *start))
-		start++;
-	while (*end && ft_strchr(set, *end) && end > start)
-		end--;
-	ptr = (char *)malloc(end++ - start + 1);
-	if (ptr)
-		ft_strlcpy(ptr, start, end - start + 1);
-	return (ptr);
+	i = 0;
+	s = ft_strlen((char *)s1);
+	while (s1[i] && ft_strchr(set, s1[i]))
+		i++;
+	while (s1[s - 1] && ft_strchr(set, s1[s - 1]))
+		s--;
+	return (ft_substr(s1, i, s - i));
 }
