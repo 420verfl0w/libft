e38d01027711175ef8b3dc0aef780dab208c2030
Samuel Tales <stales@2G5.42angouleme.fr>
2022-04-04 17:50:59 +0200

    update

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: stales <stales@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 17:59:23 by stales            #+#    #+#             */
-/*   Updated: 2022/03/31 19:06:57 by stales           ###   ########.fr       */
+/*   Updated: 2022/04/04 17:39:26 by stales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*tmp;
 
 	new = LIBFT_NULL;
-	while (lst && f && del)
+	while (lst)
 	{
-		tmp = ft_lstnew(f(lst->content));
-		if (!tmp)
+		if (f)
+			tmp = ft_lstnew(f(lst->content));
+		if (!tmp && del)
 		{
 			ft_lstclear(&tmp, del);
 			return (LIBFT_NULL);
